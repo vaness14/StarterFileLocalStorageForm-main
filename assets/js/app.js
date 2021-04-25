c6859a5af5085bd1cf0068d090b20c92c62bacba
Vanessa De La Cruz <delacruzvanessa445@gmail.com>
2021-04-25 16:07:56 -0700

    Printing elements in local storage load

--- assets/js/app.js
+++ assets/js/app.js
@@ -79,5 +79,29 @@ functionaddTweetLocalStorage(tweet) {
         }
     return tweets;
     }
+    //Prints Local Storage tweets on Load
+functionlocalStorageOnLoad() {
+    let tweets = getTweetsFromStorage();
+    
+        //Loop throught storage and then Print the values
+    tweets.forEach(function(tweet) {
+    
+        //Create the remove button
+    constremoveBtn = document.createElement('a');
+    removeBtn.classList = 'remove-tweet';
+    removeBtn.textContent = 'X';
+    
+        //Create an <li> Element
+    const li = document.createElement('li');
+    li.textContent = tweet;
+    
+        //Add the remove button to each tweet
+    li.appendChild(removeBtn);
+    
+        //Add to the list
+    tweetList.appendChild(li);
+        });
+    }
+    
     
     
