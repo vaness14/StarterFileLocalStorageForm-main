f9634c57ebee62b4e4d2402500c160d79c206978
Vanessa De La Cruz <delacruzvanessa445@gmail.com>
2021-04-25 16:03:28 -0700

    Adding new elements into DOM

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,37 @@ tweetList.addEventListener('click', removeTweet);
     //Document
 document.addEventListener('DOMContentLoaded', localStorageOnLoad);
 }
+//Functions
+
+functionnewTweet(e){
+    e.preventDefault();
+    
+        //Read Textarea value
+    const tweet = document.getElementById('tweet').value;
+    
+    
+        //Create the remove button
+    constremoveBtn = document.createElement('a');
+    removeBtn.classList = 'remove-tweet';
+    removeBtn.textContent = 'X';
+    
+        //Create an <li> Element
+    const li = document.createElement('li');
+    li.textContent = tweet;
+    
+        //Add the remove button to each tweet
+    li.appendChild(removeBtn);
+    
+        //Add to the list
+    tweetList.appendChild(li);
+    
+        //Add to local storage
+    addTweetLocalStorage(tweet);
+    
+        //Print the alert
+    alert('Tweet Added');                                                          
+    
+    this.reset();
+    
+    }
+    
